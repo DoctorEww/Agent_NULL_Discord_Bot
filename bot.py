391d21d6244c80d837c94af4065fd7c97e3b3367
DoctorEww <dgriess084@gmail.com>
2020-11-11 13:16:47 -0800

    Added a message on user join... I hope it works!

--- bot.py
+++ bot.py
@@ -20,5 +20,10 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})\n'
     )
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
