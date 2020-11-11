b6c0e0c56b24b7346a58b141c6c6556b552e5aad
DoctorEww <dgriess084@gmail.com>
2020-11-11 13:25:55 -0800

    added a few more funnys

--- bot.py
+++ bot.py
@@ -37,5 +37,11 @@ async def on_message(message):
         await message.channel.send("CHICKEN BUTT")
     if message.content == 'remove the bot':
         await message.channel.send("please I have a wife and children")
+    if message.content == 'flag':
+        await message.channel.send("My creator already sent the flag... If you cant remember it this long you don't deserve it")
+    if message.content == 'what is love':
+        await message.channel.send("baby dont hurt me")
+    if message.content == 'dont hurt me':
+        await message.channel.send("no more")
 
 client.run(TOKEN)
