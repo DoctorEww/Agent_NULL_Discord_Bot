4a8737739d1d5c353c35947bae170e276b45d55e
DoctorEww <dgriess084@gmail.com>
2020-11-11 13:15:28 -0800

    import discord and start on the discord client

--- bot.py
+++ bot.py
@@ -2,6 +2,23 @@
 # Don't take life to seriously
 # Author DoctorEww
 
+import discord
 
+TOKEN = "TODO"
+GUILD = "TODO"
 
-print("hello world!")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
