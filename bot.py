f7d039130f07118a8a3bfa1c5ab76b61297a282e
DoctorEww <dgriess084@gmail.com>
2020-11-11 13:37:06 -0800

    updated entry message

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ async def on_ready():
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
+        f'Hi {member.name}, welcome to our sensitive discord server! Do not leak any messages especially the flag...'
     )
 
 @client.event
